e4252f85d0fdceaff0ed9c5e8a7bd97296b03a72
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-11 22:14:42 -0300

    atualizaçao do lint

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const botaoEntrar = document.querySelector('.botao-entrar');
-
+const botaoSubmit = document.querySelector('#submit-btn');
+const checkAgreement = document.querySelector('#agreement');
 function login() {
   const email = document.querySelector('#email');
   const senha = document.querySelector('#senha');
-
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
-
+function habilitarBotao() {
+  if (checkAgreement.checked) {
+    botaoSubmit.removeAttribute('disabled');
+  } else {
+    botaoSubmit.setAttribute('disabled', '');
+  }
+}
+checkAgreement.addEventListener('click', habilitarBotao);
 botaoEntrar.addEventListener('click', login);
